894cda6ae8e02fd911711de3318010bd8717c010
Shreya <shreyak438@gmail.com>
2021-08-03 15:17:00 +0530

    String rotation

--- questions/rgbHexConverter.js
+++ questions/rgbHexConverter.js
@@ -2,6 +2,30 @@
  * The function takes three arguments corresponding to R,G and B values.
  * The function converts these values to corresponding HEX color code
  */
-const rgbToHexConversion = (...rgbValues) => {};
+const rgbToHexConversion = (...rgbValues) => {
+    
+    var rgbToHex = function (rgb) { 
+        var hex = Number(rgb).toString(16);
+        if(Number.isInteger(hex) && hex <= 255){
+            if ( hex.length < 2) {
+                hex = "0" + hex;
+           }
+           return hex;
+        }
+        else{
+            new Error("Invalid Input");
+        }
+        
+      };
+
+     
+        var red = rgbToHex(rgbValues[0]);
+        var green = rgbToHex(rgbValues[1]);
+        var blue = rgbToHex(rgbValues[2]);
+        return red+green+blue;
+     
+
+
+};
 
 module.exports = rgbToHexConversion;
--- questions/stringRotation.js
+++ questions/stringRotation.js
@@ -4,6 +4,33 @@ const rotateString = (str, n, direction) => {
   //
   // input str = random ,1, right
   // output mrando
+  if(typeof(str) === "string"){
+    if(direction == "right" || direction =="left"){
+      if(direction=="left"){
+        var left = str.substring(n, str.length) +
+        str.substring(0, n);
+        return left;
+      }
+
+      if(direction=="right"){
+        var right = str.substring(str.length-n, str.length) +
+        str.substring(0, str.length-n);
+        return right;
+      }
+    }
+    else{
+      throw new Error("Invalid Direction");
+    }
+      
+  }
+  else{
+    throw new Error("Invalid Input");
+  }
+     
+  
+      
+  
+  
 };
 
 module.exports = rotateString;
