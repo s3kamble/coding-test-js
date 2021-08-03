d4c3a15cf57ae711445f42e96031e1ad9cf1a877
Shreya <shreyak438@gmail.com>
2021-08-03 12:35:30 +0530

    convertTime task completed

--- questions/convertTime.js
+++ questions/convertTime.js
@@ -6,6 +6,18 @@
  The value corresponding to each key should be the conversion of seconds into correponding 
  time 
  */
-const convertTime = (seconds) => {};
+const convertTime = (seconds) => {
+    if (!isNaN(seconds)){
+        let hours = ~~( seconds / 3600) ;
+        let minutes = ~~(( seconds % 3600) / 60);
+        let second = ~~ seconds % 60 ;
+
+        return { hours:hours, minutes:minutes, seconds:second};
+    }
+    else{
+        throw new Error("Invalid Input");
+    }
+    
+};
 
 module.exports = convertTime;
